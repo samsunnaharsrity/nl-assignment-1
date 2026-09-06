270a1aa5809efe13bc7142639753712497aae029
Samsunnahar Srity <samsunnaharsrity@gmail.com>
2026-09-07 01:32:59 +0600

    clg remove

--- Que-5.js
+++ Que-5.js
@@ -23,9 +23,3 @@ const getChaseVerdict = (target, scored, ballsLeft) => {
 
   return `Need ${runsNeeded} runs in ${ballsLeft} balls | ${verdict}`;
 };
-// console.log(getChaseVerdict(200, 200, 12));
-// console.log(getChaseVerdict(200, 190, 0));
-// console.log(getChaseVerdict(100, 90, 12));
-// console.log(getChaseVerdict(100, 80, 12));
-// console.log(getChaseVerdict(100, 70, 12));
-// console.log(getChaseVerdict(150, 149, 1));
